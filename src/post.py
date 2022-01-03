26b8d47bb771ea18b383329c199ce2c50a5cf728
TrucDev <jackyhuynh87@gmail.com>
2022-01-03 11:48:28 -0500

    Update Block Project

--- src/post.py
+++
@@ -1,8 +0,0 @@
-class Post:
-    def __init__(self, title, subtitle, author, dates, body, reference):
-        self.title = title
-        self.subtitle = subtitle
-        self.author = author
-        self.dates = dates
-        self.body = body
-        self.reference = reference
